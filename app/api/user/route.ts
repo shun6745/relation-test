a79df0c10877fe5b5306a3529e3715e473b1b4ee
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-08-02 13:25:10 +0900

    認証するためのひな形作り

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,31 +1,31 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const POST = async (req: Request) => {
-  try {
-    const { name, email, posts } = await req.json();
+// export const POST = async (req: Request) => {
+//   try {
+//     const { name, email, posts } = await req.json();
 
-    // Create a new user along with posts
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        email,
-        posts: {
-          create: posts, // posts should be an array of post objects
-        },
-      },
-      include: {
-        posts: true, // Include posts in the response
-      },
-    });
-//エラー文とってもいい
-    return NextResponse.json(newUser);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
-  }
-};
+//     // Create a new user along with posts
+//     const newUser = await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//         posts: {
+//           create: posts, // posts should be an array of post objects
+//         },
+//       },
+//       include: {
+//         posts: true, // Include posts in the response
+//       },
+//     });
+// //エラー文とってもいい
+//     return NextResponse.json(newUser);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+//   }
+// };
 
 //7.29 ポストは値が取れてる
