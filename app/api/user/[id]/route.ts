e360b0909c147814189723b0d3912c3d18d3a8b9
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-08-02 11:32:04 +0900

    auth testの練習branchを発行します。

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -21,48 +21,24 @@ export const GET = async (req: Request) => {
 	}
 };
 
-// export const GET = async (
-// 	req: Request,
-// 	{ params }: { params: { id: string } },
-// 	res: NextResponse
-// ) => {
-// 	const id: number = parseInt(params.id);
+export const PUT = async (
+	req: Request,
+	{ params }: { params: { id: string } },
+	res: NextResponse
+) => {
+	const id: number = parseInt(params.id);
+	const { name, email, posts } = await req.json();
 
-// 	const user = await prisma.user.findUnique({
-// 		where: { id },
-// 		include: {
-// 			posts: true, // Include posts in the response
-// 		},
-// 	});
-
-// 	return NextResponse.json(user);
-// };
-
-// トライキャッチ文なしで考える
-
-// export const GET = async (
-// 	req: Request,
-// 	{ params }: { params: { id: string } }
-// ) => {
-// 	const { id } = params;
-// 	try {
-// 		const user = await prisma.user.findUnique({
-// 			where: { id: parseInt(id, 10) },
-// 			include: {
-// 				posts: true, // Include posts in the response
-// 			},
-// 		});
-
-// 		if (!user) {
-// 			return NextResponse.json({ error: "User not found" }, { status: 404 });
-// 		}
-
-// 		return NextResponse.json(user);
-// 	} catch (error) {
-// 		console.error(error);
-// 		return NextResponse.json(
-// 			{ error: "Something went wrong" },
-// 			{ status: 500 }
-// 		);
-// 	}
-// };
+	const user = await prisma.user.update({
+		data: {
+			name,
+			email,
+			posts: {
+				deleteMany: {}, // 既存の関連投稿をすべて削除
+				create: posts, // 新しい投稿を作成
+			},
+		},
+		where: { id },
+	});
+	return NextResponse.json(user);
+};
