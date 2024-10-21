e53cfb35a7db05d3e95c10505192e40edce87217
shun6745 <shun.sa3ka7na111@gmail.com>
2024-10-22 07:16:28 +0900

    divhrlloを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
 	return (
 		<div className="h-screen justify-center items-center">
 			<div className="flex flex-col items-center mt-2 mb-0 scroll-py-5"></div>
+			<div>test4</div>
+			<div>hrllo</div>
 			<UserDetail />
 		</div>
 	);
